d452dfacc2b83c86914044a8f37446cda0ebf8bb
IDAbyLIGL <haakonu97@gmail.com>
2020-03-08 19:59:46 +0100

    Added ImageRatioFixer

--- Toggle/src/index.js
+++ Toggle/src/index.js
@@ -1,24 +1,13 @@
-import React, { useState } from "react";
-import "./index.css";
+import React from 'react'
+import './index.css'
 
 export default props => {
-  const [checked, setChecked] = useState(false);
-  const click = event => {
-    setChecked(!checked);
-    typeof props.onChange === "function" && props.onChange(event);
-  };
   return (
     <label className="hu-comp-toggle-label">
       <span className="hu-comp-toggle-span">
-        <input
-          type="checkbox"
-          className="hu-comp-toggle-check"
-          {...props}
-          onChange={click}
-          checked={checked}
-        />
+        <input type="checkbox" className="hu-comp-toggle-check" {...props} />
       </span>
-      {props.label || ""}
+      {props.label || ''}
     </label>
-  );
-};
+  )
+}
